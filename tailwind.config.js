f7d7d91091791da055ec805f16b6118e6afcb70e
lymangos <20227666@stu.neu.edu.cn>
2025-12-03 18:05:21 +0800

    refactor: replace tailwind cdn with local build 003

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // 1. 搬过来的颜色配置
+      colors: {
+        stone: {
+          950: '#0c0a09',
+          900: '#1c1917',
+          800: '#292524',
+          700: '#44403c',
+          600: '#57534e',
+          500: '#78716c',
+          400: '#a8a29e',
+          300: '#d6d3d1',
+          200: '#e7e5e4',
+          100: '#f5f5f4',
+        }
+      },
+      // 2. 搬过来的字体配置
+      fontFamily: {
+        serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
-
